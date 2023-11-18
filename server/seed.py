18ca6ea869f8c9dcd8eb0a878a2686e97cbe748b
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-18 11:18:56 -0700

    complete lab

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,6 @@ from models import db, Article, User
 fake = Faker()
 
 with app.app_context():
-
     print("Deleting all records...")
     Article.query.delete()
     User.query.delete()
@@ -25,19 +24,19 @@ with app.app_context():
     articles = []
     for i in range(100):
         content = fake.paragraph(nb_sentences=8)
-        preview = content[:25] + '...'
-        
+        preview = content[:25] + "..."
+
         article = Article(
             author=fake.name(),
             title=fake.sentence(),
             content=content,
             preview=preview,
-            minutes_to_read=randint(1,20),
+            minutes_to_read=randint(1, 20),
         )
 
         articles.append(article)
 
     db.session.add_all(articles)
-    
+
     db.session.commit()
     print("Complete.")
